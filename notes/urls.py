02cdbf88b2143b1fb7d29c7e7f5a4db6196838a7
nonconductor <124586939+Jopkabulbi123@users.noreply.github.com>
2025-05-15 14:09:16 +0300

    html added

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_notes),
+    path('', views.note_list, name='note_list'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
 
-def hello_notes(request):
-    return HttpResponse("Hello from Notes app.")
+def note_list(request):
+    test_notes = [
+        {'title': 'Перша нотатка', 'content': 'Привіт від Django!', 'created_at': datetime.now()},
+        {'title': 'Список покупок', 'content': 'Хліб, молоко, яйця', 'created_at': datetime.now()}
+    ]
+    return render(request, 'notes/index.html', {'notes': test_notes})
